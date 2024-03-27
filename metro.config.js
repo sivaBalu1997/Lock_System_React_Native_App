c01cf25b8c86dbbe5d6f636a07294abe1df640cc
sivaBalu1997 <128580030+sivaBalu1997@users.noreply.github.com>
2024-03-27 17:54:10 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,42 +1,11 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-/*const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const defaultSourceExts = require('metro-config/src/defaults/defaults').sourceExts;
-const defaultAssetExts = require('metro-config/src/defaults/defaults').assetExts;
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
+const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), {
-    transformer: {
-        babelTransformerPath: require.resolve('react-native-svg-transformer'),
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: true,
-            },
-        }),
-    },
-    resolver: {
-        assetExts: defaultAssetExts.filter(ext => ext !== 'svg'),
-        sourceExts: [...defaultSourceExts, 'svg'],
-    },
-});*/
-
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
