b79869e5adc6887f63cf70971d194858b3aa625b
sivaBalu1997 <128580030+sivaBalu1997@users.noreply.github.com>
2024-03-27 17:29:25 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,33 @@
  *
  * @format
  */
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const defaultSourceExts = require('metro-config/src/defaults/defaults').sourceExts;
+const defaultAssetExts = require('metro-config/src/defaults/defaults').assetExts;
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 
-module.exports = {
+module.exports = mergeConfig(getDefaultConfig(__dirname), {
+    transformer: {
+        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
+    resolver: {
+        assetExts: defaultAssetExts.filter(ext => ext !== 'svg'),
+        sourceExts: [...defaultSourceExts, 'svg'],
+    },
+});
+
+/*module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -14,4 +39,4 @@ module.exports = {
       },
     }),
   },
-};
+};*/
